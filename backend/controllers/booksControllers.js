790c78f8f97ac12ed7f1b56c78f5f0caa3f7ac7f
kai <leonc4667@gmail.com>
2022-07-04 11:13:09 -0400

    api

--- backend/controllers/booksControllers.js
+++ backend/controllers/booksControllers.js
@@ -1,7 +1,50 @@
+const Book = require('../models/Book')
+
 const getBooks = async (req, res) => {
-    res.send('SDSD')
+   try {
+    const findBooks = await Book.find().lean()
+    res.status(201).json(findBooks)
+   } catch (error) {
+    console.log(error)
+    return res.status(500).send({ message: error });
+   }
 }
 
+const createBooks = async (req, res) => {  
+    try {
+        const {title, author, isbn, imagePath} = req.body
+        const newBook = new Book({
+            title,
+            author,
+            isbn,
+            imagePath,
+        })
+        const creatingBook = await newBook.save()
+        res.status(201).json(creatingBook)
+    } catch (error) {
+    console.log(error)
+    return res.status(500).send({ message: error });
+    }
+}
+
+const deleteBook = async (req, res) => {    
+    try {
+        const findBook = await Book.findById(req.params.id)
+        if (!findBook) {
+            res.status(404).json({ message: 'Book not found' })
+        } 
+        await findBook.remove()
+        res.status(201).json({ message: 'Book deleted successfully' })
+        
+    } catch (error) {
+    console.log(error)
+    return res.status(500).send({ message: error });
+    }
+}
+
+
 module.exports = {
-    getBooks
+    getBooks,
+    createBooks,
+    deleteBook
 }
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
-const {getBooks} = require('../controllers/booksControllers')
+const {getBooks, createBooks, deleteBook} = require('../controllers/booksControllers')
 const router = Router()
 
 router.get('/', getBooks)
+router.post('/', createBooks)
+router.delete('/:id', deleteBook)
 
 module.exports = router
